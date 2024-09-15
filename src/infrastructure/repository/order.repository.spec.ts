558943d129387b10515975b07ddb526b5b87d70c
Gabriel Emer <gabriel_emer@hotmail.com>
2024-09-15 19:33:32 -0300

    Implementando a interface padrão de repositorys e os métodos faltantes.

--- src/infrastructure/repository/order.repository.spec.ts
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -71,4 +71,92 @@ describe("Order repository test", () => {
     });
   });
 
+  it("should update a order", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    customer.changeAddress(new Address("Street 1", 1, "Zipcode 1", "City 1"));
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem("1", product.id, product.name, product.price, 2);
+
+    const order = new Order("123", customer.id, [orderItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+    const orderModel = await OrderModel.findOne({
+      where: { id: order.id },
+      include: ["items"],
+    });
+    expect(orderModel.toJSON()).toStrictEqual({
+      id: "123",
+      customer_id: "123",
+      total: order.total(),
+      items: [
+        {
+          id: "1",
+          name: "Product 1",
+          price: 10,
+          quantity: 2,
+          order_id: "123",
+          product_id: "123",
+        },
+      ],
+    });
+  });
+
+  it("should find a order", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    customer.changeAddress(new Address("Street 1", 1, "Zipcode 1", "City 1"));
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem("1", product.id, product.name, product.price, 2);
+
+    const order = new Order("123", customer.id, [orderItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    const orderResult = await orderRepository.findById(order.id);
+    expect(order).toStrictEqual(orderResult);
+  });
+
+  it("should find all orders", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    customer.changeAddress(new Address("Street 1", 1, "Zipcode 1", "City 1"));
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem("1", product.id, product.name, product.price, 2);
+    const orderItemTwo = new OrderItem("2", product.id, product.name, product.price, 2);
+
+    const order = new Order("123", customer.id, [orderItem]);
+    const orderTwo = new Order("1234", customer.id, [orderItemTwo]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+    await orderRepository.create(orderTwo);
+
+    const orders = await orderRepository.findAll();
+    expect(orders).toHaveLength(2);
+    expect(orders).toContainEqual(order);
+    expect(orders).toContainEqual(orderTwo);
+
+    expect(order).not.toEqual(orderTwo);
+
+    expect(order.items).not.toEqual(orderTwo.items);
+  });
+
 });
--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -1,8 +1,11 @@
 import Order from "../../domain/entity/order";
 import OrderModel from "../db/sequelize/model/order.model";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
+import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
+import OrderItem from "../../domain/entity/orderItem";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface {
+  
   async create(entity: Order): Promise<void> {
     await OrderModel.create({
       id: entity.id,
@@ -19,5 +22,48 @@ export default class OrderRepository {
       include: [{ model: OrderItemModel }],
     })
   }
+
+  async update(entity: Order): Promise<void> {
+    await OrderModel.update({
+      customer_id: entity.customerId,
+      total: entity.total(),
+      items: entity.items.map((item) => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        product_id: item.productId,
+        quantity: item.quantity
+      })),
+    }, {
+      where: {
+        id: entity.id
+      }
+    })
+  }
+
+  async findById(id: string): Promise<Order> {
+    const orderModel = await OrderModel.findOne({
+      where: { id },
+      include: [{ model: OrderItemModel }],
+      rejectOnEmpty: true,
+    });
+
+    const orderItems = orderModel.items.map((item) => this.toOrderItem(item));
+    return new Order(orderModel.id, orderModel.customer_id, orderItems);
+  }
   
+  async findAll(): Promise<Order[]> {
+    const orderModels = await OrderModel.findAll({
+      include: [{ model: OrderItemModel }],
+    });
+
+    return orderModels.map((orderModel) => {
+      const orderItems = orderModel.items.map((item) => this.toOrderItem(item));
+      return new Order(orderModel.id, orderModel.customer_id, orderItems);
+    });
+  }
+
+  private toOrderItem(item: OrderItemModel): OrderItem {
+    return new OrderItem(item.id, item.product_id, item.name, item.price, item.quantity);
+  }
 }
